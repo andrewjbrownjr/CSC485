f18c4fc1bd1bc91a520897fff6d114baaf396d61
andrewjbrownjr <103289768+andrewjbrownjr@users.noreply.github.com>
2023-10-03 09:05:02 -0400

    Separated tests

--- CSC485/tests/hw10/test_hw10.py
+++ CSC485/tests/hw10/test_hw10.py
@@ -1,9 +1,20 @@
+import pytest
 from CSC485.projects.hw10 import fruit_query
 
-def test_answers():
+def test_lowercase():
   assert fruit_query.is_it_a_fruit('apple') == True
+
+def test_uppercase():
   assert fruit_query.is_it_a_fruit('PEAR') == True
+
+def test_some_uppercase():
   assert fruit_query.is_it_a_fruit('Banana') == True
+
+def test_not_fruit():
   assert fruit_query.is_it_a_fruit('lettuce') == False
+
+def test_bool():
   assert fruit_query.is_it_a_fruit(True) == False
+
+def test_int():
   assert fruit_query.is_it_a_fruit(5) == False
