c93f11e10168f82a12a7a0d71b6d4ecd552f2bef
andrewjbrownjr <103289768+andrewjbrownjr@users.noreply.github.com>
2023-10-13 17:34:49 -0400

    fixing project structure/hw11

--- CSC485/tests/hw11/test_hw11.py
+++ CSC485/tests/hw11/test_hw11.py
@@ -1,51 +1,45 @@
 import pytest
 from CSC485.projects.hw11 import formal_names
+'''
+'''
+class TestFormalNames:
+    '''
+    Tests for get_formal_name from part 1
+    '''
+    @pytest.mark.parametrize('fruit1',['apple','Apple','APPLE','aPpLe'])
+    def test_is_a_fruit_1(self, fruit1):
+        '''
+        Method to test if get_formal_name returns the formal name of
+        a fruit with a fruit as the input string
+        '''
+        assert formal_names.get_formal_name(fruit1) == 'Malus domestica'
 
-#Part 1 test
-@pytest.mark.parametrize('key',['apple','Apple','APPLE','aPpLe'])
-def test_apple_1(key):
-    '''
-    Method to test if get_formal_name returns the formal name of
-    an apple with apple as the input string
-    '''
-    assert formal_names.get_formal_name(key) == 'Malus domestica'
+
+    def test_not_a_fruit_1(self):
+        '''
+        Method to test if get_formal_name raises a KeyError when
+        a string that is not the name of a fruit is input
+        '''
+        with pytest.raises(KeyError):
+            formal_names.get_formal_name('this is not a fruit')
 
 
-def test_not_a_fruit_1():
+class TestFormalNames2:
     '''
-    Method to test if get_formal_name raises a KeyError when
-    a string that is not the name of a fruit is input
+    Tests for get_formal_name_2 from part 2
     '''
-    with pytest.raises(KeyError):
-        formal_names.get_formal_name('this is not a fruit')
+    @pytest.mark.parametrize('fruit2',['apple','APPLE','Apple','aPpLe'])
+    def test_is_a_fruit_2(self, fruit2):
+        '''
+        Method to test if get_formal_name_2 returns the formal name of
+        a fruit when the input is a fruit
+        '''
+        assert formal_names.get_formal_name_2(fruit2) == 'Malus domestica'
 
-#Part 2 tests
-@pytest.mark.parametrize('key',['apple','APPLE','Apple','aPpLe'])
-def test_apple_2(key):
-    '''
-    Method to test if get_formal_name_2 returns the formal name of
-    an apple when the input is an apple
-    '''
-    assert formal_names.get_formal_name_2(key) == 'Malus domestica'
-
-def test_uppercase_2():
-    '''
-    Method to test if get_formal_name_2 returns the formal name of
-    a fruit when the input is an all uppercase fruit name
-    '''
-    assert formal_names.get_formal_name_2('APPLE') == 'Malus domestica'
-
-def test_some_uppercase_2():
-    '''
-    Method to test if get_formal_name_2 returns the formal name of a fruit when
-    the input is a fruit name with some uppercase and some lowercase letters
-    '''
-    assert formal_names.get_formal_name_2('Apple') == 'Malus domestica'
-
-def test_not_a_fruit_2():
-    '''
-    Method to test if get_formal_name_2 raises a KeyError when
-    the input is not the name of a valid fruit
-    '''
-    with pytest.raises(KeyError):
-        formal_names.get_formal_name_2('this is not a fruit')
+    def test_not_a_fruit_2(self):
+        '''
+        Method to test if get_formal_name_2 raises a KeyError when
+        the input is not the name of a valid fruit
+        '''
+        with pytest.raises(KeyError):
+            formal_names.get_formal_name_2('this is not a fruit')
--- CSC485/tests/hw12/test_hw12.py
+++ CSC485/tests/hw12/test_hw12.py
@@ -7,42 +7,46 @@ containing different complexifiers, different string lengths, and
 different sequences of characters in the strings.
 '''
 
-@pytest.mark.parametrize('key',['@','~~~','##','$$$$$$','_______','----','+=+=+'])
-def test_all_complexifiers(key):
+class TestComputeComplexity:
     '''
-    Method to test that compute_complexity returns 100 when
-    the input string is only complexifiers
+    These methods will test compute_complexity
     '''
-    assert compute_complexity(key) == 100
+    @pytest.mark.parametrize('key',['@','~~~','##','$$$$$$','_______','----','+=+=+'])
+    def test_all_complexifiers(self, key):
+        '''
+        Method to test that compute_complexity returns 100 when
+        the input string is only complexifiers
+        '''
+        assert compute_complexity(key) == 100
 
-@pytest.mark.parametrize('key',['a@','#e$e','%%tt','op-_','@@gg@@gg','~~~lll'])
-def test_half_complexifiers(key):
-    '''
-    Method to test that compute_complexity returns 50
-    when half of the input string is complexifiers
-    '''
-    assert compute_complexity(key) == 50
+    @pytest.mark.parametrize('key',['a@','#e$e','%%tt','op-_','@@gg@@gg','~~~lll'])
+    def test_half_complexifiers(self, key):
+        '''
+        Method to test that compute_complexity returns 50
+        when half of the input string is complexifiers
+        '''
+        assert compute_complexity(key) == 50
 
-@pytest.mark.parametrize('key',['@aaa','##eeeeee','oo#o','p-pp','&&&aaaaaaaaa'])
-def test_quarter_complexifiers(key):
-    '''
-    Method to test that compute_complexity returns 25
-    when a quarter of the string is complexifiers
-    '''
-    assert compute_complexity(key) == 25
+    @pytest.mark.parametrize('key',['@aaa','##eeeeee','oo#o','p-pp','&&&aaaaaaaaa'])
+    def test_quarter_complexifiers(self, key):
+        '''
+        Method to test that compute_complexity returns 25
+        when a quarter of the string is complexifiers
+        '''
+        assert compute_complexity(key) == 25
 
-@pytest.mark.parametrize('key',['@aaaaaaaaa','w#wwwwwwww','jj_jjjjjjj','ooooooooo$'])
-def test_tenth_complexifiers(key):
-    '''
-    Method to test that compute_complexity returns 10 when
-    a tenth of the input string is complexifiers
-    '''
-    assert compute_complexity(key) == 10
+    @pytest.mark.parametrize('key',['@aaaaaaaaa','w#wwwwwwww','jj_jjjjjjj','ooooooooo$'])
+    def test_tenth_complexifiers(self, key):
+        '''
+        Method to test that compute_complexity returns 10 when
+        a tenth of the input string is complexifiers
+        '''
+        assert compute_complexity(key) == 10
 
-@pytest.mark.parametrize('key',['k','aaaaaaaaaa','llll','00000'])
-def test_no_complexifiers(key):
-    '''
-    Method to test that compute_complexity returns 0 when
-    none of the input string is complexifiers
-    '''
-    assert compute_complexity(key) == 0
+    @pytest.mark.parametrize('key',['k','aaaaaaaaaa','llll','00000'])
+    def test_no_complexifiers(self, key):
+        '''
+        Method to test that compute_complexity returns 0 when
+        none of the input string is complexifiers
+        '''
+        assert compute_complexity(key) == 0
